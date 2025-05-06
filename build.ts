101a477d551864a1279e16b33a4b9ae16ac5e6eb
tehlordvortex <me@vrtx.sh>
2025-05-06 10:47:28 +0100

    chore: add binary compilation

--- build.ts
+++ build.ts
@@ -1,10 +1,35 @@
+// https://bun.sh/docs/bundler/executables#cross-compile-to-other-platforms
+const BUN_BINARY_TARGETS = [
+	"linux-x64",
+	"linux-arm64",
+	"linux-x64-musl",
+	"linux-arm64-musl",
+	"windows-x64",
+	"darwin-x64",
+	"darwin-arm64",
+];
+
 await Bun.$`rm -rf ./dist`;
-await Bun.build({
-  minify: true,
-  target: "node",
-  outdir: "./dist",
-  format: "cjs",
-  sourcemap: "external",
-  banner: "#!/usr/bin/env node",
-  entrypoints: ["./src/index.ts"],
-});
+
+if (process.argv[2] === "compile") {
+	await Bun.$`mkdir -p ./dist`;
+
+	for (const target of BUN_BINARY_TARGETS) {
+		let suffix = target.replaceAll("-", "_");
+		if (target.includes("windows")) {
+			suffix += ".exe";
+		}
+
+		await Bun.$`bun build --target=bun-${target} --compile --minify --sourcemap --bytecode src/index.ts --outfile=dist/rover_mcp_${suffix}`;
+	}
+} else {
+	await Bun.build({
+		minify: true,
+		target: "node",
+		outdir: "./dist",
+		format: "cjs",
+		sourcemap: "external",
+		banner: "#!/usr/bin/env node",
+		entrypoints: ["./src/index.ts"],
+	});
+}
